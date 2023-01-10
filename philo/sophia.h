65c6371f41fde42fd2251763464aefb9cec5caec
Omar Bednaoui <obednaou@e2r5p11.1337.ma>
2023-01-10 21:46:17 +0100

    philosophy

---
+++ philo/sophia.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sophia.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obednaou <obednaou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/10 21:42:36 by obednaou          #+#    #+#             */
+/*   Updated: 2023/01/10 21:43:38 by obednaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SOPHIA_H
+# define SOPHIA_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+
+#endif
